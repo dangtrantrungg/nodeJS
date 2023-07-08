34195e7cd38a092532a8dbb24ac5ffb68ff45156
Dang Tran Trung <dangtrantrung0501@gmail.com>
2023-07-08 11:59:04 +0700

    create navigator bar

--- src/controler/homeControler.js
+++ src/controler/homeControler.js
@@ -5,12 +5,16 @@ let getHomePage = (req, res) => {
     connection.query(
         'SELECT * FROM `user`',
         function (err, results, fields) {
-            data = results.map((row) => { return row })
-            return res.render('index.ejs', { dataUser: JSON.stringify(data) })
-        }
-    );
+            data = results.map((row) => {
+                let id = row.id;
+                let firstName = row.firstname;
+                let lastName = row.lastname;
+                let email = row.email;
+                return row
+            });
+            return res.render('index.ejs', { dataUser: data })
+        })
 }
-
 module.exports = {
     getHomePage
 }
